c3065ef7c621f2f84a660f0cc133c0e6258f6902
Johandry Amador <johandry@yahoo.com>
2019-11-14 16:49:11 -0800

    Chapter 05 (complete)

--- Labs/Ch05/README.md
+++ Labs/Ch05/README.md
@@ -1,10 +1,17 @@
 # Chapter 5: Deployment Configuration
 
-**Documentation**
+##### Documentation
 
-kubernetes.io -> Tasks -> Configure Pods and Containers -> [Configure a Pod to Use a ConfigMap](https://kubernetes.io/docs/tasks/configure-pod-container/configure-pod-configmap/)
+- kubernetes.io -> Tasks -> Configure Pods and Containers -> [Configure a Pod to Use a ConfigMap](https://kubernetes.io/docs/tasks/configure-pod-container/configure-pod-configmap/)
+- kubernetes.io -> Concepts -> Configuration -> [Secrets](https://kubernetes.io/docs/concepts/configuration/secret/)
+- kubernetes.io -> Tasks -> Inject Data Into Applications -> [Distribute Credentials Securely Using Secrets](https://kubernetes.io/docs/tasks/inject-data-application/distribute-credentials-secure/)
+- kubernetes.io -> Tasks -> Configure Pods and Containers -> [Configure a Pod to Use a Volume for Storage](https://kubernetes.io/docs/tasks/configure-pod-container/configure-volume-storage/)
+- kubernetes.io -> Tasks -> Configure Pods and Containers -> [Configure a Pod to Use a PersistentVolume for Storage](https://kubernetes.io/docs/tasks/configure-pod-container/configure-persistent-volume-storage/)
+- kubernetes.io -> Concepts -> Workloads -> Controllers -> [Deployments](https://kubernetes.io/docs/concepts/workloads/controllers/deployment/)
 
-**Create ConfigMap from values, files and directories**
+#### ConfigMaps
+
+##### Create ConfigMap from values, files and directories
 
 ```bash
 echo -e "key1=value1\nkey2=value2" > /path/to/file
@@ -15,7 +22,7 @@ kubectl create configmap config \
 	--from-file=/path/to/dir/
 ```
 
-**View ConfigMap**
+##### View ConfigMap
 
 ```bash
 kubectl get configmap config -o yaml
@@ -23,7 +30,7 @@ kubectl get configmap config -o yaml
 kubectl describe configmap config
 ```
 
-**Use the ConfigMap as environment variable**
+##### Use the ConfigMap as environment variable
 
 ```bash
 kubectl run nginx --image=nginx --restart=Never --dry-run -o yaml > pod.yaml
@@ -58,11 +65,323 @@ kubectl create -f pod.yaml
 kubectl exec -it nginx -- env
 ```
 
-**Use the ConfigMap as volume**
+##### Use the ConfigMap as volume
+
+```bash
+vi pod.yaml
+```
+
+```yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  creationTimestamp: null
+  labels:
+    run: nginx
+  name: nginx
+spec:
+  containers:
+  - image: nginx
+    name: nginx
+    resources: {}
+    env:
+    - name: config				# env var name
+    	valueFrom:
+    		configMapKeyRef:
+    			name: config 		# configmap name
+    			key: akey				# key name in the cm. If ignored, will get all
+    volumeMounts:
+    - name: my-config-vol	# reference to name of the volume, see below
+    	mountPath: /etc/config	# path inside the container
+  volume:
+  - name: my-config-vol		# name of the volume
+  	configMap:
+  		name: config				# name of the configMap
+  dnsPolicy: ClusterFirst
+  restartPolicy: Never
+status: {}
+```
+
+```bash
+kubectl exec -it nginx -- /bin/sh -c 'ls -l /etc/config && cat /etc/config/key1'
+```
+
+#### Secrets
+
+##### Create Secret with manifest
+
+Create the base64-encoded output for the data
+
+```bash
+echo -n "superpassword" | base64
+```
+
+Include it in the `Secret.data` object. Create it from scratch using the documentation.
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+	name: demo-secret
+data:
+	password: c3VwZXJwYXNzd29yZA==
+```
+
+##### Create Secret from literal and file
+
+```bash
+echo -n "admin" > username
+
+kubectl create secret generic demo-secret-1 \
+	--from-literal=password='c3VwZXJwYXNzd29yZA==' \
+	--from-file=username
+
+kubectl get secret demo-secret-1 \
+	-o jsonpath='{.data.username}{"\n"}' | base64 -d
+```
+
+##### Use secrets in a container with volume
+
+Create the Pod or Deployment
+
+```bash
+kubectl run nginx --image=nginx --restart=Never -o yaml --dry-run > pod.yaml
+```
+
+Add the volume to `spec.volumes` and mount it in the container with `spec.containers[n].volumeMounts`
+
+```yaml
+apiVersion: v1
+kind: Pod
+...
+spec:
+	volumes:
+	- name: demo-secret-volume
+		secret:
+			secretName: demo-secret-1
+	containers:
+	- name: nginx
+		volumeMounts:
+		- name: demo-secret-vm
+			mountPath: /etc/secret
+		...
+```
+
+Check
+
+```bash
+kubectl exec nginx -- /bin/bash -c \
+	'ls -l /etc/secret && cat /etc/secret/username && cat /etc/secret/password'
+```
+
+It prints the files `username` and `password`, then `admin` and `c3VwZXJwYXNzd29yZA==`.
+
+##### Use secrets in a container with environment variables
+
+Add in the Pod into `spec.containers[n].env`
+
+```yaml
+apiVersion: v1
+kind: Pod
+...
+spec:
+	containers:
+	- name: test-container
+		env:
+		- name: USERNAME
+			valueFrom:
+				secretKeyRef: 
+					name: demo-secret-1
+					key: username
+		- name: PASSWORD
+			valueFrom:
+				secretKeyRef:
+					name: demo-secret-1
+					key: password
+		...
+```
+
+Check
+
+```bash
+kubectl exec nginx -- /bin/bash -c 'echo $USERNAME:$PASSWORD'
+```
+
+#### Volumes
+
+1. Create the Presistent Volume
+2. Create the Persistent Volume Claim
+3. Add the Volume to the Pod and to the Container
+
+##### Create the Presistent Volume
+
+It's created from scratch, get help from the kubernetes.io documentation
+
+```yaml
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+	name: demo-pv-volume
+	labels:
+		type: local
+spec:
+	storageClassName: manual
+	capacity:
+		storage: 10Gi
+	accessMode:
+		- ReadWriteOnce
+	hostPath:
+		path: "/mnt/data"
+```
+
+If you are in Kubernetes on Docker Desktop, make sure the `path` is in the File Sharing list.
+
+##### Create the Persistent Volume Claim
+
+It's created from scratch, get help from the kubernetes.io documentation
+
+```yaml
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+	name: demo-pv-claim
+spec:
+	storageClassName: manual		# use the same class name as the PV
+	accessModes:
+		- ReadWriteOnce						# use one of the mode used by the PV
+	resources:
+		request:
+			storage: 3Gi						# make sure is =< than the PV capacity
+```
+
+When created, make sure the status is `Bound`.
+
+##### Add the Volume to the Pod and to the Container
+
+Generate the code for the Pod or Deployment:
+
+```bash
+kubectl run nginx --image=nginx --restart=Never -o yaml --dry-run > pod.yaml
+```
+
+Add the volume to the Pod in `Pod.spec.volumes`
+
+```yaml
+apiVersion: v1
+kind: Pod
+...
+spec:
+	volumes:
+		- name: demo-pv-storage
+			persistentVolumeClaim:
+				claimName: demo-pv-claim
+	containers:
+	...
+```
+
+Add the volume to the Container in `Pod.spec.containers[n].volumeMounts`
+
+```yaml
+apiVersion: v1
+kind: Pod
+...
+spec:
+	containers:
+		- name: nginx
+			volumeMounts:
+				- mountPath: "/var/log/nginx"
+					name: demo-pv-storage 	# same name as in volumes section
+			...
+```
+
+To check the content of the volume inside the container use:
+
+```bash
+kubectl exec nginx -it -- ls /var/log/nginx
+```
+
+In your local system:
+
+```bash
+ls /mnt/data
+```
+
+They should have the same data.
+
+#### Rollbacks
+
+Use allways version tags in the images, try to avoid `:latest` tag.
+
+To update the pod or deployment use `kubectl set` or `kubectl edit`.
+
+```bash
+export KUBE_EDITOR=vim
+kubectl edit deployment demo
+```
+
+Or 
+
+```bash
+kubectl set image deployment demo simpleapp=simpleapp:v2
+```
+
+`kubectl set image TYPE NAME CONTAINER1=IMAGE1 CONTAINER2=IMAGE2 ...`
+
+##### To verify the status
+
+```bash
+kubectl rollout status deployment demo
+```
+
+##### Undo
+
+```bash
+kubectl rollout undo deployment demo --dry-run
+
+kubectl rollout undo deployment demo
+```
+
+##### Undo to specific version
+
+```bash
+kubectl rollout history deployment demo
+kubectl rollout undo deployment demo --revision=1
+```
+
+##### View rollout changes
+
+```bash
+kubectl describe deployment demo | grep Image:
+# Also
+diff <(kubectl rollout history deploy demo --revision=1) \
+		 <(kubectl rollout history deploy demo --revision=2)
+```
+
+##### Scale a deployment
+
+```bash
+kubectl scale deployment demo --replicas=5
+```
+
+##### Autoscale 
+
+Between 5-10 pods, autoscale when CPU arrives to 80%
+
+```bash
+kubectl autoscale deployment demo --min=5 --max=10 --cpu-percent=80
+kubectl get hpa
+```
+
+#### Pause/resume the rollout
+
+```bash
+kubectl rollout pause deployment demo
+kubectl rollout resume deployment demo
+```
 
 
 
-## From Taining
+## From the Training
 
 **emptyDir** storage:
 
--- Labs/Ch05/solution.sh
+++ Labs/Ch05/solution.sh
@@ -206,7 +206,327 @@ EOD
   kubectl exec -it -c simpleapp $POD -- /bin/bash -c 'cat /etc/cars/car.*'
 }
 
+create_volumes() {
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Persistent Volume:\n"
+  cat <<EOPV | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-vol-1
+  labels:
+    type: local
+spec:
+  storageClassName: manual
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteMany
+  hostPath:
+    path: "/tmp/data"
+EOPV
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m List the Persistent Volumes:\n"
+  kubectl get pv
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Persistent Volume Claim:\n"
+  cat <<EOPVC | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: pvc-1
+spec:
+  storageClassName: manual
+  accessModes:
+    - ReadWriteMany
+  resources:
+    requests:
+      storage: 200Mi
+EOPVC
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m List the Persistent Volumes:\n"
+  kubectl get pvc
+}
+
+use_volume() {
+  kubectl delete deployment try
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Deployment:\n"
+  cat <<EOD | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  labels:
+    app: try
+  name: try
+spec:
+  progressDeadlineSeconds: 600
+  replicas: 6
+  selector:
+    matchLabels:
+      app: try
+  template:
+    metadata:
+      labels:
+        app: try
+    spec:
+      containers:
+      - image: johandry/simpleapp:latest
+        volumeMounts:
+        - mountPath: /etc/cars
+          name: car-vol
+        - mountPath: /tmp
+          name: tmp-vol
+        env:
+        - name: ilike
+          valueFrom:
+            configMapKeyRef:
+              name: colors
+              key: favorite
+        envFrom:
+        - configMapRef:
+            name: colors
+        imagePullPolicy: Always
+        name: simpleapp
+        readinessProbe:
+          periodSeconds: 5
+          exec:
+            command:
+            - ls
+            - /etc/cars
+      - image: k8s.gcr.io/goproxy:0.1
+        imagePullPolicy: Always
+        name: goproxy
+        ports:
+        - containerPort: 8080
+        readinessProbe:
+          tcpSocket:
+            port: 8080
+          initialDelaySeconds: 5
+          periodSeconds: 10
+        livenessProbe:
+          tcpSocket:
+            port: 8080
+          initialDelaySeconds: 15
+          periodSeconds: 20
+      volumes:
+      - name: car-vol
+        configMap:
+          defaultMode: 420
+          name: car
+      - name: tmp-vol
+        persistentVolumeClaim:
+          claimName: pvc-1
+      restartPolicy: Always
+EOD
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m List Pods:\n"
+  watch -n 5 kubectl get pods
+  kubectl get pods
+
+  POD=$(kubectl get pods | grep try | head -1 | awk '{ print $1 }')
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Describe Pod $POD:\n"
+  kubectl describe pod $POD
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Check Volume from pod $POD:\n"
+  kubectl exec -it -c simpleapp $POD -- /bin/sh -c 'ls -al /tmp/'
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create file in volumen of pod $POD:\n"
+  kubectl exec -it -c simpleapp $POD -- /bin/sh -c 'echo "test from pod" > /tmp/test.txt'
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Check file in volumen from localhost:\n"
+  cat /tmp/data/test.txt
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Update file from localhost:\n"
+  echo "second test" >> /tmp/data/test.txt
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Checking file in volumen from pod $POD:\n"
+  kubectl exec -it -c simpleapp $POD -- /bin/sh -c 'cat /tmp/test.txt'
+}
+
+create_basicpod() {
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create local host path:\n"
+  mkdir -f /tmp/weblog
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Persistent Volume:\n"
+  cat <<EOP | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: weblog-pv-vol
+  labels:
+    type: local
+spec:
+  storageClassName: manual
+  capacity:
+    storage: 100Mi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: "/tmp/weblog"
+EOP
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Verify Persistent Volume:\n"
+  kubectl get pv weblog-pv-vol
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Persistent Volume Claim:\n"
+  cat <<EOP | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: weblog-pv-claim
+spec:
+  storageClassName: manual
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 100Mi
+EOP
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Verify Persistent Volume Claim:\n"
+  kubectl get pvc weblog-pv-claim
+
+  kubectl delete cm fluentd-config
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create ConfigMap for fluentd:\n"
+  cat <<EOCM | kubectl create -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: fluentd-config
+data:
+  fluentd.conf: |
+    <source>
+      @type tail
+      format none
+      path /var/log/nginx/access.log 
+      tag count.format1
+    </source>
+
+    <match *.**> 
+      @type forward
+
+      <server>
+        name localhost 
+        host 127.0.0.1
+      </server>
+    </match>
+EOCM
+
+  kubectl delete pod basicpod
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Create Pod with Volume & config'ed fluentd:\n"
+  cat <<EOP | kubectl create -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: basicpod
+  labels:
+    type: webserver
+spec:
+  containers:
+  - name: webcont
+    image: nginx
+    ports:
+      - containerPort: 80
+    volumeMounts:
+      - mountPath: "/var/log/nginx"
+        name: weblog-pv-storage
+  - name: fdlogger
+    image: fluent/fluentd
+    env:
+    - name: FLUENTD_ARGS
+      value: -c /etc/fluentd-config/fluentd.conf
+    volumeMounts:
+      - mountPath: "/var/log"
+        name: weblog-pv-storage
+      - mountPath: "/etc/fluentd-config"
+        name: log-config
+  volumes:
+  - name: weblog-pv-storage
+    persistentVolumeClaim:
+      claimName: weblog-pv-claim
+  - name: log-config
+    configMap:
+      name: fluentd-config
+EOP
+  watch -n 5 kubectl get po basicpod
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Verify local host storage:\n"
+  ls -al /tmp/weblog
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Verify Pod storage in both containers:\n"
+  kubectl exec -it -c webcont  basicpod -- /bin/bash -c 'ls -al /var/log/nginx'
+  kubectl exec -it -c fdlogger basicpod -- /bin/sh   -c 'ls -al /var/log'
+
+  IP=$(kubectl get po basicpod -o wide | grep basicpod | awk '{print $6}')
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Verify the web service:\n"
+  kubectl run busybox --rm -it --restart=Never --image=busybox -- wget -O- $IP:80
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Check pod logs:\n"
+  kubectl logs basicpod fdlogger
+}
+
+docker_build() {
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Modify the code or the Docker image:\n"
+  sed 's/sleep(5)/sleep(8)/' ../Ch03/simple.py > simple.py
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Docker build image:\n"
+  cat <<EOD | docker build -t simpleapp -f - .
+FROM python:2
+ADD simple.py /
+CMD [ "python", "./simple.py" ]
+EOD
+  
+  rm -f simple.py
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Docker images:\n"
+  docker images simpleapp
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Tag image as version 2:\n"
+  docker tag simpleapp johandry/simpleapp:v2
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Pushing version 2:\n"
+  docker push johandry/simpleapp:v2
+}
+
+rollout() {
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Modify the code or the Docker image:\n"
+  # export KUBE_EDITOR=vim
+  # kubectl edit deployment try
+  kubectl set image deploy try simpleapp=johandry/simpleapp:v2
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m View update:\n"
+  watch -n 5 kubectl get pods
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m View events:\n"
+  kubectl get events
+
+  POD=$(kubectl get pods | grep try | head -1 | awk '{print $1}')
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m View images in pod $POD:\n"
+  kubectl describe pod try-7b7f46984c-7ggmg | grep Image:
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Rollout history:\n"
+  kubectl rollout history deployment try
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Different between revision 1 and 2:\n"
+  diff <(kubectl rollout history deployment try --revision=1) <(kubectl rollout history deployment try --revision=2)
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m What whould be done if we restore replica #1:\n"
+  kubectl rollout undo deployment try --dry-run
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m Rollback to replica #1:\n"
+  kubectl rollout undo deployment try --to-revision=1
+
+  echo -ne "\n\x1B[92;1m[INFO ]\x1B[0m View rollback:\n"
+  watch -n 5 kubectl get pods
+}
+
 # create_configmap
 # use_configmap
 # create_configmap_from_manifest
-use_configmap_from_manifest
+# use_configmap_from_manifest
+# create_volumes
+use_volume
+# create_basicpod
+# docker_build
+rollout
